728bc1e91d4b626e9a83ab880c02c6fc5e696ada
Mihai CICU <T0232181@ad-01.ent-01.adgroup>
2019-10-03 12:09:40 +0300

    Added downloader from mp4box.js example

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
-var MP4Box = require('mp4box');
+
 var mp4boxfile = MP4Box.createFile();
+
+mp4boxfile.onMoovStart = function () {
+	console.log("Starting to receive File Information");
+}
+
+mp4boxfile.onReady = function (info) {
+	console.log("Received File Information");
+}
